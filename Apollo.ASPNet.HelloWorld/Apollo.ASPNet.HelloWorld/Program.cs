95c17126c79bf9ca2b2db2c4529969911016bf62
sbalanchickoo <36227562+sbalanchickoo@users.noreply.github.com>
2018-05-27 03:16:05 +0100

    comment

--- Apollo.ASPNet.HelloWorld/Apollo.ASPNet.HelloWorld/Program.cs
+++ Apollo.ASPNet.HelloWorld/Apollo.ASPNet.HelloWorld/Program.cs
@@ -15,6 +15,7 @@ namespace Apollo.ASPNet.HelloWorld
         public static void Main(string[] args)
         {
             BuildWebHost(args).Run();
+            //comment
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
