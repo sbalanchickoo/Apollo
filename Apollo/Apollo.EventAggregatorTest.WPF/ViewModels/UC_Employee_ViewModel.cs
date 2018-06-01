c4aedcd2b30fbfb839a45bcba74a548af01fa722
balansrikant <36227601+balansrikant@users.noreply.github.com>
2018-06-01 05:20:19 +0100

    Project renamed

---
+++ Apollo/Apollo.EventAggregatorTest.WPF/ViewModels/UC_Employee_ViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Events;
+
+
+namespace EventAggregatorTest.ViewModels
+{
+    class UC_Employee_ViewModel
+    {
+        public UC_Employee_ViewModel(IEventAggregator iEventAggregator)
+        {
+            this.iEventAggregator = iEventAggregator;
+            this.PersonDetails = new List<Person>();
+            this.PersonDetails.Add(new Person() { Address = "Hyderabad", Name = "Hari", Id = 1, Photo = "/PrismEventAggregator;component/Resources/Images/img_chania.jpg" });
+            this.PersonDetails.Add(new Person() { Address = "Guntur", Name = "Murali", Id = 2, Photo = "/PrismEventAggregator;component/Resources/Images/img_chania2.jpg" });
+            this.PersonDetails.Add(new Person() { Address = "Ongole", Name = "Varun", Id = 3, Photo = "/PrismEventAggregator;component/Resources/Images/img_flower2.jpg" });
+        }
+    }
+}
---
+++ Apollo/Apollo.GradeStatistics.CS/GradeBook.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeStatistics
+{
+    public class GradeBook
+    {
+        private string _Name;
+
+    }
+}
