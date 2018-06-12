4e4df5701057fef0b2fb6e0bc012e32d18b01a48
Srikant Balan <sbalanchickoo@gmail.com>
2018-06-12 17:47:49 +0100

    interface defined

---
+++ Apollo/Apollo.Interface.CS/Interfaces/PolygonInterface.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Apollo.Interface.CS.Interfaces
+{
+    public interface PolygonInterface
+    {
+
+        int NumberOfSides { get; set; }
+        int SideLength { get; set; }
+
+        int GetPerimeter();
+
+        int GetArea();
+
+
+    }
+}
