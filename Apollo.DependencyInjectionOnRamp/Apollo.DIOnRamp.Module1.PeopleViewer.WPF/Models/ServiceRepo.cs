7ee23e79455ec95a98abb11c0221edd793ae8ea1
balansrikant <36227601+balansrikant@users.noreply.github.com>
2018-07-11 12:44:19 +0100

    WIP

--- Apollo.DependencyInjectionOnRamp/Apollo.DIOnRamp.Module1.PeopleViewer.WPF/Models/ServiceRepo.cs
+++ Apollo.DependencyInjectionOnRamp/Apollo.DIOnRamp.Module1.PeopleViewer.WPF/Models/ServiceRepo.cs
@@ -1,33 +1,33 @@
-﻿using Apollo.DIOnRamp.Module1.PeopleViewer.WPF.ServiceReference1;
-using Apollo.DIOnRamp.Module1.Shared.Interfaces;
-using Apollo.DIOnRamp.Module1.Shared.Models;
-using System;
-using System.Collections.Generic;
+﻿////using Apollo.DIOnRamp.Module1.PeopleViewer.WPF.ServiceReference1;
+//using Apollo.DIOnRamp.Module1.Shared.Interfaces;
+//using Apollo.DIOnRamp.Module1.Shared.Models;
+//using System;
+//using System.Collections.Generic;
 
-namespace Apollo.DIOnRamp.Module1.PeopleViewer.WPF.Models
-{
-    public class ServiceRepo : IPersonRepository
-    {
-        PeopleServiceClient peopleService = new PeopleServiceClient();
+//namespace Apollo.DIOnRamp.Module1.PeopleViewer.WPF.Models
+//{
+//    public class ServiceRepo : IPersonRepository
+//    {
+//        //PeopleServiceClient peopleService = new PeopleServiceClient();
 
-        public void AddPerson()
-        {
-            peopleService.AddPerson();
-        }
+//        public void AddPerson()
+//        {
+//            peopleService.AddPerson();
+//        }
 
-        public void DeletePerson(string lastName, string firstName)
-        {
-            throw new NotImplementedException();
-        }
+//        public void DeletePerson(string lastName, string firstName)
+//        {
+//            throw new NotImplementedException();
+//        }
 
-        public IEnumerable<Person> GetPeople()
-        {
-            return peopleService.GetPeople();
-        }
+//        public IEnumerable<Person> GetPeople()
+//        {
+//            return peopleService.GetPeople();
+//        }
 
-        public Person GetPerson()
-        {
-            throw new NotImplementedException();
-        }
-    }
-}
+//        public Person GetPerson()
+//        {
+//            throw new NotImplementedException();
+//        }
+//    }
+//}
