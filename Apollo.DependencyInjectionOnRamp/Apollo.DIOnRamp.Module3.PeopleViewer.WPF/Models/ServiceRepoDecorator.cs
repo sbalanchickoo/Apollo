86aa1e2f0b3aac52bcdf0680e9a8080de67967c7
Srikant Balan <36227794+shribee@users.noreply.github.com>
2018-07-11 18:31:04 +0100

    WIP

---
+++ Apollo.DependencyInjectionOnRamp/Apollo.DIOnRamp.Module3.PeopleViewer.WPF/Models/ServiceRepoDecorator.cs
@@ -0,0 +1,42 @@
+﻿using Apollo.DIOnRamp.Module3.PeopleViewer.WPF.ServiceReference3;
+using Apollo.DIOnRamp.Module3.Shared.Interfaces;
+using Apollo.DIOnRamp.Module3.Shared.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Apollo.DIOnRamp.Module3.PeopleViewer.WPF.Models
+{
+    public class ServiceRepoDecorator : IPersonRepository
+    {
+        PeopleServiceClient peopleService = new PeopleServiceClient();
+
+        private int _durationSec;
+
+        public int DurationSec
+        {
+            get { return _durationSec; }
+            set { _durationSec = value; }
+        }
+
+
+        public void AddPerson()
+        {
+            peopleService.AddPerson();
+        }
+
+        public void DeletePerson(string lastName, string firstName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Person> GetPeople()
+        {
+            return peopleService.GetPeople();
+        }
+
+        public Person GetPerson()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
