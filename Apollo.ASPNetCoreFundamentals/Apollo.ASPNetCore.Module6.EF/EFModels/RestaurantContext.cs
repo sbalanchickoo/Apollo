82a90cf2d6184f9e15442e5360eeccc14cec8d98
Srikant Balan <balansrikant@gmail.com>
2018-08-23 12:26:51 +0100

    WIP

---
+++ Apollo.ASPNetCoreFundamentals/Apollo.ASPNetCore.Module6.EF/EFModels/RestaurantContext.cs
@@ -0,0 +1,18 @@
+﻿using Apollo.ASPNetCore.Module6.RestaurantReviews.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Apollo.ASPNetCore.Module6.RestaurantReviews.EFModels
+{
+    public class RestaurantContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurant { get; set; }
+        public DbSet<Cuisine> Cuisine { get; set; }
+        public DbSet<Greeting> Greeting { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+        {
+            dbContextOptionsBuilder.UseSqlServer(
+                "Server = (localdb)\\MSSQLLocalDB; Initial Catalog = master; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False");
+        }
+    }
+}
